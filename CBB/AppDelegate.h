0c9838be17e93750d4792892d803c7dc762e18b6
卡宝宝 <Sirius@kabaotakaratekiMac-mini.local>
2013-08-06 09:44:29 +0800

    Initial Commit

---
+++ CBB/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CBB
+//
+//  Created by 卡宝宝 on 13-8-6.
+//  Copyright (c) 2013年 卡宝宝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CBB/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CBB
+//
+//  Created by 卡宝宝 on 13-8-6.
+//  Copyright (c) 2013年 卡宝宝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
