75feaea5bf1e1a7d36558f9a8071473227af5c6a
Young <royal_yzh@hotmail.com>
2014-02-17 17:52:15 +0800

    20140217

---
+++ CBB/iPhone/View/LoanView/LoanHomeController.h
@@ -0,0 +1,13 @@
+//
+//  LoanHomeController.h
+//  CBB
+//
+//  Created by 卡宝宝 on 14-2-17.
+//  Copyright (c) 2014年 卡宝宝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoanHomeController : UIViewController
+
+@end
---
+++ CBB/iPhone/View/LoanView/LoanHomeController.m
@@ -0,0 +1,38 @@
+//
+//  LoanHomeController.m
+//  CBB
+//
+//  Created by 卡宝宝 on 14-2-17.
+//  Copyright (c) 2014年 卡宝宝. All rights reserved.
+//
+
+#import "LoanHomeController.h"
+
+@interface LoanHomeController ()
+
+@end
+
+@implementation LoanHomeController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
